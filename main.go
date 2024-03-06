f23d8f9149890951a30240bf97b51be27d815e5c
Luke Marsden <me@lukemarsden.net>
2024-03-06 13:54:56 +0000

    wip

--- main.go
+++ main.go
@@ -2,22 +2,111 @@ package main
 
 import (
 	"context"
+	"fmt"
 )
 
-type Gcp struct {}
+// TODO: rename GCR -> Artifact Registry (GAR?)
 
-// example usage: "dagger call container-echo --string-arg yo"
-func (m *Gcp) ContainerEcho(stringArg string) *Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
-}
+type Gcp struct{}
 
-// example usage: "dagger call grep-dir --directory-arg . --pattern GrepDir"
-func (m *Gcp) GrepDir(ctx context.Context, directoryArg *Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
+// example usage: "dagger call get-secret --gcp-credentials ~/.config/gcloud/application_default_credentials.json"
+func (m *Gcp) GetSecret(ctx context.Context, gcpCredentials *File) (string, error) {
+	ctr, err := m.WithGcpSecret(ctx, dag.Container().From("ubuntu:latest"), gcpCredentials)
+	if err != nil {
+		return "", err
+	}
+	return ctr.
+		WithExec([]string{"bash", "-c", "cat /root/.config/gcloud/application_default_credentials.json |base64"}).
 		Stdout(ctx)
 }
 
+func (m *Gcp) WithGcpSecret(ctx context.Context, ctr *Container, gcpCredentials *File) (*Container, error) {
+	credsFile, err := gcpCredentials.Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+	secret := dag.SetSecret("gcp-credential", credsFile)
+	return ctr.WithMountedSecret("/root/.config/gcloud/application_default_credentials.json", secret), nil
+}
+
+func (m *Gcp) GcpCli(ctx context.Context, gcpCredentials *File) (*Container, error) {
+	ctr := dag.Container().
+		From("gcr.io/google.com/cloudsdktool/google-cloud-cli:latest")
+	ctr, err := m.WithGcpSecret(ctx, ctr, gcpCredentials)
+	if err != nil {
+		return nil, err
+	}
+	return ctr, nil
+}
+
+// example usage: "dagger call list --account your@email.address --gcp-credentials ~/.config/gcloud/application_default_credentials.json"
+func (m *Gcp) List(ctx context.Context, account string, gcpCredentials *File) (string, error) {
+	ctr, err := m.GcpCli(ctx, gcpCredentials)
+	if err != nil {
+		return "", err
+	}
+	return ctr.
+		WithExec([]string{"gcloud", "--account", account, "compute", "instances", "list"}).
+		Stdout(ctx)
+}
+
+// example usage: "dagger call gcr-get-login-password --region us-east-1 --gcp-credentials ~/.config/gcloud/application_default_credentials.json"
+func (m *Gcp) GcrGetLoginPassword(ctx context.Context, gcpCredentials *File, region string) (string, error) {
+	ctr, err := m.GcpCli(ctx, gcpCredentials)
+	if err != nil {
+		return "", err
+	}
+	// TODO: what is this?
+	return ctr.
+		WithExec([]string{"--region", region, "ecr", "get-login-password"}).
+		Stdout(ctx)
+}
+
+// Push ubuntu:latest to GCR under given repo 'test' (repo must be created first)
+// example usage: "dagger call gcr-push-example --region us-east-1 --gcp-credentials ~/.config/gcloud/application_default_credentials.json --gcp-account-id 12345 --repo test"
+func (m *Gcp) GcrPushExample(ctx context.Context, gcpCredentials *File, region, gcpProject, repo, image string) (string, error) {
+	ctr := dag.Container().From("ubuntu:latest")
+	return m.GcrPush(ctx, gcpCredentials, region, gcpProject, repo, image, ctr)
+}
+
+func (m *Gcp) GcrPush(ctx context.Context, gcpCredentials *File, region, gcpProject, repo, image string, ctr *Container) (string, error) {
+	// Get the GCR login password so we can authenticate with Publish WithRegistryAuth
+	ctr, err := m.GcpCli(ctx, gcpCredentials)
+	if err != nil {
+		return "", err
+	}
+	/*
+
+		PROJECT_ID="your-project-id"
+		SERVICE_ACCOUNT_NAME="kube-registry-sa"
+		DISPLAY_NAME="Kube Registry SA"
+
+		gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME \
+			--display-name="$DISPLAY_NAME" \
+			--project=$PROJECT_ID
+
+		gcloud projects add-iam-policy-binding $PROJECT_ID \
+			--member="serviceAccount:$SERVICE_ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
+			--role="roles/artifactregistry.reader"
+
+		gcloud iam service-accounts keys create sa-key.json \
+			--iam-account="$SERVICE_ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
+			--project=$PROJECT_ID
+
+	*/
+	// TODO: what is this?
+	regCred, err := ctr.
+		WithExec([]string{"--region", region, "ecr", "get-login-password"}).
+		Stdout(ctx)
+	if err != nil {
+		return "", err
+	}
+	// secret will be a service account json
+	secret := dag.SetSecret("gcp-reg-cred", regCred)
+	// region e.g. europe-west2
+	gcrHost := fmt.Sprintf("%s-docker.pkg.dev", region)
+	// e.g europe-west2-docker.pkg.dev/<project-id>/<artifact-repository>/<docker-image>
+	gcrWithRepo := fmt.Sprintf("%s/%s/%s/%s", gcrHost, gcpProject, repo, image)
+
+	return ctr.WithRegistryAuth(gcrHost, "_json_key_base64", secret).Publish(ctx, gcrWithRepo)
+}
