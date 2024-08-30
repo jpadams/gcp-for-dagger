661493a28763f75b3932ef29a5a670abff73ecc0
vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
2024-08-31 03:16:05 +0530

    Update main.go

--- main.go
+++ main.go
@@ -100,10 +100,12 @@ func (m *Gcp) GarEnsureServiceAccountKey(ctx context.Context, account, region, p
 
 			if err != nil {
 				// create failed
+				fmt.Println("create failed")
 				return "", err
 			}
 		} else {
 			// some other error getting policy, propagate it
+				fmt.Println("something else failed")
 			return "", err
 		}
 	}
